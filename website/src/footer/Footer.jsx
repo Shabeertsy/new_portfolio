8733fd20db0b9e8108faa33f38c414d9a4907ca5
shabeer_p_p <shabeer@zanyautomation.com>
2023-10-22 13:18:04 +0530

    make it responsive and add footer

--- website/src/footer/Footer.jsx
+++ website/src/footer/Footer.jsx
@@ -1,9 +1,122 @@
 import React from 'react';
+import './Footer.css'
 
-export default function  () {
+export default function() {
+  const footerStyle = {
+    backgroundColor: '#23242a',
+  };
+
+  const inputContainerStyle = {
+    marginLeft: '0px',
+  };
+
+  const ulStyle = {
+    marginLeft: '1.65em',
+  };
+
+  const notchMiddleStyle = {
+    width: '48.8px',
+  };
+
   return (
-   <div className="">
-    
-   </div>
+    <div className="footer-main">
+      <div className="container my-5">
+        <footer className="text-white text-center text-lg-start" style={footerStyle}>
+          <div className="container p-4">
+            <div className="row mt-4">
+              <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+                <h5 className="text-uppercase mb-4">About company</h5>
+                <p>
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+                  voluptatum deleniti atque corrupti.
+                </p>
+                <p>
+                  Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias.
+                </p>
+                <div className="mt-4">
+                  <a type="button" className="btn m-2 btn-floating btn-warning btn-lg">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a type="button" className="btn m-2  btn-floating btn-warning btn-lg">
+                    <i className="fab fa-dribbble"></i>
+                  </a>
+                  <a type="button" className="btn m-2  btn-floating btn-warning btn-lg">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a type="button" className="btn m-2  btn-floating btn-warning btn-lg">
+                    <i className="fab fa-google-plus-g"></i>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
+                <div className="form-outline form-white mb-4">
+                  <input type="text" id="formControlLg" className="form-control form-control-lg" />
+                  <label className="form-label" htmlFor="formControlLg" style={inputContainerStyle}>
+                    Search
+                  </label>
+                  <div className="form-notch">
+                    <div className="form-notch-leading" style={{ width: '9px' }}></div>
+                    <div className="form-notch-middle" style={notchMiddleStyle}></div>
+                    <div className="form-notch-trailing"></div>
+                  </div>
+                </div>
+                <ul className="fa-ul" style={ulStyle}>
+                  <li className="mb-3">
+                    <span className="fa-li">
+                      <i className="fas fa-home"></i>
+                    </span>
+                    <span className="ms-2">New York, NY 10012, US</span>
+                  </li>
+                  <li className="mb-3">
+                    <span className="fa-li">
+                      <i className="fa fa-envelope"></i>
+                    </span>
+                    <span className="ms-2">info@example.com</span>
+                  </li>
+                  <li className="mb-3">
+                    <span className="fa-li">
+                      <i className="fas fa-phone"></i>
+                    </span>
+                    <span className="ms-2">+ 01 234 567 88</span>
+                  </li>
+                  <li className="mb-3">
+                    <span className="fa-li">
+                      <i className="fas fa-print"></i>
+                    </span>
+                    <span className="ms-2">+ 01 234 567 89</span>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 className="text-uppercase mb-4">Opening hours</h5>
+                <table className="table text-center text-white">
+                  <tbody className="font-weight-normal">
+                    <tr>
+                      <td>Mon - Thu:</td>
+                      <td>8am - 9pm</td>
+                    </tr>
+                    <tr>
+                      <td>Fri - Sat:</td>
+                      <td>8am - 1am</td>
+                    </tr>
+                    <tr>
+                      <td>Sunday:</td>
+                      <td>9am - 10pm</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+            © 2023 Copyright:
+            <a className="text-white" href="">
+              myweb
+            </a>
+          </div>
+        </footer>
+      </div>
+    </div>
   );
-};
+}
