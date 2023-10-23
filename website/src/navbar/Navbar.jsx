f21fe0b24e8acf0aa2cabf22cdd81cb916bf24e7
shabeer_p_p <shabeer@zanyautomation.com>
2023-10-23 17:19:03 +0530

    navbar btn color change

--- website/src/navbar/Navbar.jsx
+++ website/src/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ function CollapsibleExample() {
       <Container>
         <Navbar.Brand className='text-light links' href="#home">PORTFOLIO</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav"  className='bg-light'/>
-        <Navbar.Collapse id="responsive-navbar-nav "  className='bg-light'>
+        <Navbar.Collapse id="responsive-navbar-nav " >
           <Nav className="me-auto">
             <Nav.Link className='text-light links'  as={Link} to='/about'>About Me</Nav.Link>
             <Nav.Link className='text-light links' as={Link}  to="#pricing">Projects</Nav.Link>
